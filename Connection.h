6077735d2c4a58d2f432dc4c96574461f6991670
jeccey <super3e5@gmail.com>
2013-12-19 17:04:11 +0800

    Create Connection.h

---
+++ Connection.h
@@ -0,0 +1,35 @@
+#ifndef LIBEVENT_CONNECTIONH
+#define LIBEVENT_CONNECTIONH
+
+extern "C" {
+	#include <event2/event.h>
+	#include <event2/buffer.h>
+	#include <event2/bufferevent.h>
+} 
+#include <string>
+
+class Connection {
+public:
+	struct bufferevent* bev;
+	evutil_socket_t fd;
+	void* server;
+
+	Connection(evutil_socket_t fd, struct bufferevent* bev, void* server);
+	void send(const char* data, size_t numBytes);
+};
+
+
+inline Connection::Connection(evutil_socket_t fd, bufferevent* bev, void* server)
+{
+	this->bev = bev;
+	this->fd = fd;
+	this->server = server;
+	printf("Created connection with server ref: %p\n", server);
+}
+
+inline void Connection::send(const char* data, size_t numBytes) {
+	if(bufferevent_write(bev, data, numBytes) == -1) {
+		printf("Error while sending in Connection::send()\n");
+	}
+}
+#endif
